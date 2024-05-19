0bd67c3ee7c3bfc5539c785a7b0cd09474230d4a
anjanshrestha8 <anjanshrestha2023@gmail.com>
2024-05-19 18:26:30 +0545

    calculator added

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [input, setInput] = useState();
+
+  const inputValue = (event) => {
+    setInput(input.concat(event.target.value));
+  };
+  return (
+    <>
+      <div className="calculator-wrapper">
+        <div className="calculator">
+          <form action="">
+            <div className="display">
+              <input type="text" value={input} />
+            </div>
+            <div>
+              <input type="button" value="AC" />
+              <input type="button" value="DE" />
+              <input type="button" value="." onClick={inputValue} />
+              <input type="button" value="/" onClick={inputValue} />
+            </div>
+            <div>
+              <input type="button" value="7" onClick={inputValue} />
+              <input type="button" value="8" onClick={inputValue} />
+              <input type="button" value="9" onClick={inputValue} />
+              <input type="button" value="*" onClick={inputValue} />
+            </div>
+            <div>
+              <input type="button" value="4" onClick={inputValue} />
+              <input type="button" value="5" onClick={inputValue} />
+              <input type="button" value="6" onClick={inputValue} />
+              <input type="button" value="+" onClick={inputValue} />
+            </div>
+            <div>
+              <input type="button" value="1" onClick={inputValue} />
+              <input type="button" value="2" onClick={inputValue} />
+              <input type="button" value="3" onClick={inputValue} />
+              <input type="button" value="-" onClick={inputValue} />
+            </div>
+            <div>
+              <input type="button" value="00" onClick={inputValue} />
+              <input type="button" value="0" onClick={inputValue} />
+              <input type="button" value="=" className="equals-to" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
